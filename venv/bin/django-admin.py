88030721166f19e6340436ce9c9fc81c1d08fa9a
9werayut <werayut.ku@gmail.com>
2019-08-08 03:45:27 +0700

    Login - Django Authentication Tutorial - Part 1-3

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/9werayut/Workspaces/Developer/Tranning/Python/Django/django-auth-example/venv/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
