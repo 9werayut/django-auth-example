a5c4f7feb65edcbeb2406b4438bf506fd2b0f1f6
9werayut <werayut.ku@gmail.com>
2019-08-08 21:49:37 +0700

    Logout - Django Authentication Tutorial - Part 4

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,6 @@ from mysite.core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("signup/", views.signup, name="signup"),
-    path('account/', include('django.contrib.auth.urls'))
+    path('account/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
